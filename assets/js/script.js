fc99b650f54564e93254060dee4b77403c964fea
ayeong0377 <ayeong0377@gmail.com>
2026-02-13 12:22:21 +0900

    @media 540 event-date, event-text2 h2, p font-size 수정, event-content 너비, margin 수정, event-card 너비 수정

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 
-var imageSrc = '/assets/images/map-icon.svg', // 마커이미지의 주소입니다    
+var imageSrc = './assets/images/map-icon.svg', // 마커이미지의 주소입니다    
     imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
@@ -91,4 +91,4 @@ var marker = new kakao.maps.Marker({
 marker.setMap(map); 
 
 // aos 
-        AOS.init();
+AOS.init();
